85b6a4947e76f8a598ed8fd0d1979d57bff99b09
Eslam Magdi <eslam.magdi.fcis@gmail.com>
2021-04-23 14:26:17 +0200

    add auth0 domain info and users info

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
+Domain Name: 
+    https://dev-nbp9gxvq.us.auth0.com/authorize?audience=CastingAgencyAPI&response_type=token&client_id=Og4u4YV5Tbz0ciOVgHXVhDP5VnFUlNcB&redirect_uri=http:/localhost:8080/login-results
 
+Username:
+    castingassistant@gmail.com
+Password:
+    BJnc9$Mgj3mYaw%V
+
+Username:
+    castingdirector@gmail.com
+Password:
+    BJnc9$Mgj3mYaw%V
+
+Username:
+    executiveproducer@gmail.com
+Password:
+    BJnc9$Mgj3mYaw%V
